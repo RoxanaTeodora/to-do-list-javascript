e74e0f676d17eb58bc60836660594dae2ecabdc9
Roxana Teodora <catana.roxana97@gmail.com>
2024-03-20 18:23:06 +0200

    disabled button

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# TODOLIST
+## TODOLIST
 
-//cerinte
-// the add must be enabled when user type in input
-// create 2 variabile container and input and button elemets
-//add an event listener for input element(try using 'input' event-if doesn't work properly tru using keyup event)
-//disabled property will be set to true when input value lenght>0 otherwise
-
-//the user should be able to add a new todo to the list clicking on add
-// -event listener -addButton
-// -create li element containing hte value from input
-//-style the li
-//-clear selection from input
-
-Steps:
-
-1.HTML & CSS Starte code
+#HTML & CSS Starte code:
 
 div container
 h1 - Todo List
@@ -22,19 +8,25 @@ input type text
 button - disabled by default
 The "Add" must be enabled when user types in input
 
-2.if the user clears the input, the button should be disabled again
+#JavaScript:
+
+disabled & enabled button
+-The "Add" button must be enabled when user types in input
+if the user clears the input, the button should be disabled again
 create 2 variables containing the input and button elements
 add an event listener for input element(try using 'input' event - if doesn't work properly try using 'keyup' event )
-disabled property will be set to true when input value lenght > 0 otherwise false
-The user should be able to add a new todo to the list
+disabled property will be set to false when input value lenght > 0 otherwise true
+The user should be able to add a new to do to the list
 
-3.event listener - addButton
+add a new todo to the list
+-The user should be able to add a new todo to the list
+Event listener - addButton
 create li element containing value from input
 append li element to list(ul)
-add styles for li
 clear selection from input
 The use should be able to delete a todo
 
-4.the creation of li element must include a button with X content
+-Edit the todo
+the creation of li element must include a button with X content
 an event listener must be added when the button is created
 the li that contains the button must be removed when delete button is clicked
--- app.js
+++ app.js
@@ -1,132 +1,35 @@
-//varianta 1
-// var todoInput = document.getElementById("todo-input");
-// var todoList = document.getElementById("todo-list");
-// var addItemsButten = document.getElementById("add-task");
-
-// todoInput.addEventListener("click", adaugareProdusLaLista);
-
-// function adaugareProdusLaLista(e) {
-//   var inputValue = input.value;
-//   console.log(inputValue);
-//   var productListItem = document.createElement("li");
-//   productListItem.innerHTML = inputValue;
-//   console.log(productListItem);
-//   productListElement.appendChild(productListItem); //appendChild modificam DOM in mod real
-// }
-
 const input = document.getElementById("todo-input");
-const addButton = document.getElementById("add-task");
+const btn = document.getElementById("add-task");
 const list = document.getElementById("todo-list");
-
-//task 1:
-// if the user clears the input, the button should be disabled again
-// create 2 variables containing the input and button elements
-// add an event listener for input element(try using 'input' event - if doesn't work properly try using 'keyup' event )
-// disabled property will be set to true when input value lenght > 0 otherwise false
-// The user should be able to add a new todo to the list
 
 input.addEventListener("input", onTypeTodo);
 
-//functiile care actioneaza la evenimente descrie ce face user-ul
+//disabled & enabled button
 function onTypeTodo() {
-  //   console.log(input.value);
-  //   console.log(addButton.disabled);//output disable is true
-  //disable=false activeaza butonul
-  //   addButton.disabled=input.value.length===0;
-  addButton.disabled = input.value.trim().length === 0;
-  //sau cu operator ternal
-  //pt a scapa de spatii se adauga trim
-  //   addButton.disabled = input.value.trim().length === 0? true: false;
-  //doar schimbam starea butonului
+  const inputValue = input.value; // Obținem valoarea câmpului de intrare
+  console.log(inputValue);
+  console.log(btn.disabled); // cand scriem in input afiseaza ca butonul este disabled in consola
+  if (inputValue.trim().length === 0) {
+    // Verificăm lungimea valorii
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+  // btn.disabled = input.value.length === 0 ? true : false;
 }
 
-// Task 2
-// event listener - addButton
-// create li element containing value from input
-// append li element to list(ul)
-// add styles for li
-// clear selection from input
-// The use should be able to delete a todo
+//add to to
+btn.addEventListener("click", onAddtodo);
 
-addButton.addEventListener("click", onAddTodo);
-function onAddTodo() {
+function onAddtodo() {
+  //se creeaza un element li
   const li = document.createElement("li");
-  // const li = document.createElement(input.value);
-  li.textContent = input.value;
+  //adaugam valoarea din input la li
+  li.innerHTML = input.value;
+  //adaugam elementul la lista
   list.appendChild(li);
-  //   input.value = "";
-  //   addButton.disabled = true; //fortat
-  clearInput();
+  //clear selection in input
+  input.value = "";
+  //disabled again
+  btn.disabled = true;
 }
-//se adauga textul intr-un h2
-//textContent echiv lui innerHtml
-
-//cod modular
-function createListItem(name) {
-  const li = document.createElement("li");
-  const title = document.createElement("p");
-  title.textContent = name;
-
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "x";
-  deleteButton.classList.add("detele-button");
-  deleteButton.addEventListener("click", onDeleteTodo);
-
-  li.appendChild(title);
-  li.appendChild(deleteButton);
-
-  return li;
-}
-// functia createListItem(name) adauga un element pe care il returneaza cu tot cu nume
-
-function clearInput() {
-  input.value = " ";
-  addButton.disabled = true;
-}
-
-function onDeleteTodo(e) {
-  e.target.parentElement.remove();
-}
-
-//varianta finala
-
-// const input = document.getElementById("todo-input");
-// const addButton = document.getElementById("add-task");
-// const list = document.getElementById("todo-list");
-
-// input.addEventListener("input", onTypeTodo);
-// addButton.addEventListener("click", onAddTodo);
-
-// function onTypeTodo() {
-//   addButton.disabled = input.value.trim().length === 0;
-// }
-
-// function onAddTodo() {
-//   const li = createListItem(input.value);
-//   list.appendChild(li);
-//   clearInput();
-// }
-
-// function createListItem(name) {
-//   const li = document.createElement("li");
-//   const title = document.createElement("p");
-//   title.textContent = name;
-
-//   const deleteButton = document.createElement("button");
-//   deleteButton.textContent = "X";
-//   deleteButton.classList.add("delete-button");
-//   deleteButton.addEventListener("click", onDeleteTodo);
-
-//   li.appendChild(title);
-//   li.appendChild(deleteButton);
-//   return li;
-// }
-
-// function clearInput() {
-//   input.value = "";
-//   addButton.disabled = true;
-// }
-
-// function onDeleteTodo(e) {
-//   e.target.parentElement.remove();
-// }
